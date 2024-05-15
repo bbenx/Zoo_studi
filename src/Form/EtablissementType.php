18d6af803dff9d75e42d91bcdd2f109643ef7ea0
Bendevfs <158311445+Bendevfs@users.noreply.github.com>
2024-05-15 20:31:14 +0200

    fin de cours 15/10

---
+++ src/Form/EtablissementType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etablissement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtablissementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('creationDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('modificationDate', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Etablissement::class,
+        ]);
+    }
+}
