22746e1bc865d18cdcf66f4a24cdd6402d091344
Bendevfs <158311445+Bendevfs@users.noreply.github.com>
2024-05-15 20:51:02 +0200

    oubli des dernieres sauvegardes

--- src/Form/EtablissementType.php
+++ src/Form/EtablissementType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Etablissement;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +14,14 @@ class EtablissementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
+            ->add('nom', TextType::class)
+            ->add('description', TextareaType::class)
             ->add('creationDate', null, [
                 'widget' => 'single_text',
             ])
             ->add('modificationDate', null, [
                 'widget' => 'single_text',
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
