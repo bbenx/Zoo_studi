998c0b0abec60834f1106e7ad1be87a653d763d6
Bendevfs <158311445+Bendevfs@users.noreply.github.com>
2024-06-10 10:04:23 +0200

    test

--- public/index.php
+++ public/index.php
@@ -1,12 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/Users/ben/project/Zoo_studi/public/index.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
