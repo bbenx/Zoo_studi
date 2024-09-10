60a122c3700ed58397d278d10dca07a2953dca6a
bbenx <b.bodin47@gmail.com>
2024-09-10 12:15:02 +0200

    fix click

--- src/Controller/Admin/AnimalClickController.php
+++ src/Controller/Admin/AnimalClickController.php
@@ -17,7 +17,6 @@ use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
 
 #[Route('admin')]
-#[IsGranted('ROLE_ADMIN')]
 class AnimalClickController extends AbstractController
 {
     private $tokenManager;
@@ -32,23 +31,30 @@ class AnimalClickController extends AbstractController
     #[Route('/animal-click/{id}', name: 'animal_click', methods: ['POST'])]
     public function click(int $id, DocumentManager $dm, EntityManagerInterface $em, Request $request): Response
     {
+
+        if ($this->getUser()) {
+            // Si l'utilisateur est connecté, on ne fait rien
+            return new Response('Click not recorded for connected users', 204);
+        }
+
         $limiter = $this->animalClickLimiter->create($request->getClientIp());
         $limit = $limiter->consume();
 
         if (!$limit->isAccepted()) {
             return new JsonResponse(['error' => 'Too many requests'], 429);
         }
-
+        error_log('Rate limiter passed.');
         $submittedToken = $request->request->get('_csrf_token');
         if (!$this->tokenManager->isTokenValid(new CsrfToken('click-animal', $submittedToken))) {
             return new Response('Invalid CSRF token', 403);
         }
+        error_log('CSRF token validation passed.');
 
         $animal = $em->getRepository(Animaux::class)->find($id);
         if (!$animal) {
             return new Response('Animal not found', 404);
         }
-
+        error_log('Animal not found.');
             $animalClick = new AnimalClick();
             $animalClick->setAnimalId($id);
             $animalClick->setPrenom($animal->getPrenom());
@@ -61,6 +67,7 @@ class AnimalClickController extends AbstractController
     }
 
     #[Route('/animal-clicks', name: 'admin_animal_clicks')]
+    #[IsGranted('ROLE_ADMIN')] 
     public function index(DocumentManager $dm, EntityManagerInterface $em): Response
     {
         // Récupération de tous les clics d'animaux
